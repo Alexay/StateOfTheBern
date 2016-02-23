ff00925f4bdf9e78a96c0b1be037fda55a8b4b69
Alexay <731@tuta.io>
2016-02-23 17:21:25 +0800

    Parsed the JSON, need synchronous writing

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var request = require('request');
+var jsonfile = require('jsonfile');
+var util = require('util');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -57,5 +60,45 @@ app.use(function(err, req, res, next) {
   });
 });
 
+sCarolinaPID = 4167;
+nevadaPID = 5337;
+texasPID = 4158;
+//georgiaPID = 5623;
+massachusettsPID = 3891;
+virginiaPID = 3922;
+minnesotaPID = 3585;
+tennesseePID = 5768;
+arkansasPID = 5233;
+oklahomaPID = 5739;
+vermontPID = 5796;
 
+statesArray = [
+    ['sCarolina', 'nevada', 'texas', 'massachusetts', 'virginia', 'minnesota', 'tennessee', 'arkansas', 'oklahoma', 'vermont'],
+    [4167,5337,4158,3891,3922,3585,5768,5233,5739,5796]
+];
+
+for (var i = 0; i<statesArray[0].length; i++)
+{
+    console.log(statesArray[0][i]);
+    console.log(statesArray[1][i]);
+    var preview_suffix = '';
+    var myUrl = 'http://www.realclearpolitics.com/epolls/json/' + statesArray[1][i] + '_historical' + preview_suffix + '.js?' + '';
+    var jsonObject;
+    var file = './public/javascripts/'+ statesArray[0][i] +'.json';
+
+
+        request({
+            url: myUrl,
+            json: true,
+        }, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(body); // Print the json response
+                jsonObject = JSON.parse(body.substr(12, body.length - 14));
+                jsonfile.writeFile(file, jsonObject, function (err) {
+                    console.error(err);
+                })
+            }
+        });
+
+}
 module.exports = app;
