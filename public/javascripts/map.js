c0470698cf92065aa688de545ac37276ab225352
Alexay <731@tuta.io>
2016-02-26 12:51:19 +0800

    Added primary/caucus date info

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -10,7 +10,7 @@ var election = new Datamap({
         highlightBorderColor: '#ffffff',
         highlightFillOpacity: 0.5,
         popupTemplate: function(geography, data) {
-            var output = '<div class="hoverinfo">' + geography.properties.name + ' delegates: ' +  data.electoralVotes;
+            var output = '<div class="hoverinfo">' + geography.properties.name + ' delegates: ' +  data.electoralVotes + '<br>Primary/caucus date: ' + data.primaryDate;
             if (data.frontrunnerName != undefined) {
                 output += '<br>' + data.frontrunnerName +' lead: ' + data.lead+'%' + '<br>' + data.pollDate;
             }
@@ -36,216 +36,267 @@ var election = new Datamap({
     data:{
         "AZ": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 75
+            "electoralVotes": 75,
+            "primaryDate": "March 22"
         },
         "CO": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 66
+            "electoralVotes": 66,
+            "primaryDate": "March 1"
         },
         "DE": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 21
+            "electoralVotes": 21,
+            "primaryDate": "April 26"
         },
         "FL": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 214
+            "electoralVotes": 214,
+            "primaryDate": "March 15"
         },
         "GA": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 102
+            "electoralVotes": 102,
+            "primaryDate": "March 1"
         },
         "HI": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 25
+            "electoralVotes": 25,
+            "primaryDate": "March 26"
         },
         "ID": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 23
+            "electoralVotes": 23,
+            "primaryDate": "March 22"
         },
         "IL": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 156
+            "electoralVotes": 156,
+            "primaryDate": "March 15"
         },
         "IN": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 83
+            "electoralVotes": 83,
+            "primaryDate": "May 3"
         },
         "IA": {
             "fillKey": "Republican Win",
             "electoralVotes": 44,
             "frontrunnerName": "Clinton",
             "lead": 0.3,
-            "pollDate": "Final result"
+            "pollDate": "Final result",
+            "primaryDate": "February 1"
         },
         "KS": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 33
+            "electoralVotes": 33,
+            "primaryDate": "March 5"
         },
         "KY": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 55
+            "electoralVotes": 55,
+            "primaryDate": "May 17"
         },
         "LA": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 51
+            "electoralVotes": 51,
+            "primaryDate": "March 5"
         },
         "MD": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 95
+            "electoralVotes": 95,
+            "primaryDate": "April 26"
         },
         "ME": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 25
+            "electoralVotes": 25,
+            "primaryDate": "March 6"
         },
         "MA": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 91
+            "electoralVotes": 91,
+            "primaryDate": "March 1"
         },
         "MN": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 77
+            "electoralVotes": 77,
+            "primaryDate": "March 1"
         },
         "MI": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 130
+            "electoralVotes": 130,
+            "primaryDate": "March 8"
         },
         "MS": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 36
+            "electoralVotes": 36,
+            "primaryDate": "March 8"
         },
         "MO": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 71
+            "electoralVotes": 71,
+            "primaryDate": "March 15"
         },
         "MT": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 21
+            "electoralVotes": 21,
+            "primaryDate": "June 7"
         },
         "NC": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 107
+            "electoralVotes": 107,
+            "primaryDate": "March 15"
         },
         "NE": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 25
+            "electoralVotes": 25,
+            "primaryDate": "March 5"
         },
         "NV": {
             "fillKey": "Republican Win",
             "electoralVotes": 35,
             "frontrunnerName": "Clinton",
             "lead": 5.3,
-            "pollDate": "Final result"
+            "pollDate": "Final result",
+            "primaryDate": "February 20"
         },
         "NH": {
             "fillKey": "Democrat Win",
             "electoralVotes": 24,
             "frontrunnerName": "Sanders",
             "lead": 22.4,
-            "pollDate": "Final result"
+            "pollDate": "Final result",
+            "primaryDate": "February 9"
         },
         "NJ": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 126
+            "electoralVotes": 126,
+            "primaryDate": "June 7"
         },
         "NY": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 247
+            "electoralVotes": 247,
+            "primaryDate": "April 19"
         },
         "ND": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 18
+            "electoralVotes": 18,
+            "primaryDate": "June 7"
         },
         "NM": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 34
+            "electoralVotes": 34,
+            "primaryDate": "June 7"
         },
         "OH": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 143
+            "electoralVotes": 143,
+            "primaryDate": "March 15"
         },
         "OK": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 38
+            "electoralVotes": 38,
+            "primaryDate": "March 1"
         },
         "OR": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 61
+            "electoralVotes": 61,
+            "primaryDate": "May 17"
         },
         "PA": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 189
+            "electoralVotes": 189,
+            "primaryDate": "April 26"
         },
         "RI": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 24
+            "electoralVotes": 24,
+            "primaryDate": "April 26"
         },
         "SC": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 53
+            "electoralVotes": 53,
+            "primaryDate": "February 27"
         },
         "SD": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 20
+            "electoralVotes": 20,
+            "primaryDate": "June 7"
         },
         "TN": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 67
+            "electoralVotes": 67,
+            "primaryDate": "March 1"
         },
         "TX": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 222
+            "electoralVotes": 222,
+            "primaryDate": "March 1"
         },
         "UT": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 33
+            "electoralVotes": 33,
+            "primaryDate": "March 22"
         },
         "WI": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 86
+            "electoralVotes": 86,
+            "primaryDate": "April 5"
         },
         "VA": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 95
+            "electoralVotes": 95,
+            "primaryDate": "March 1"
         },
         "VT": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 16
+            "electoralVotes": 16,
+            "primaryDate": "March 1"
         },
         "WA": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 101
+            "electoralVotes": 101,
+            "primaryDate": "March 26"
         },
         "WV": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 29
+            "electoralVotes": 29,
+            "primaryDate": "May 10"
         },
         "WY": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 14
+            "electoralVotes": 14,
+            "primaryDate": "April 9"
         },
         "CA": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 475
+            "electoralVotes": 475,
+            "primaryDate": "June 7"
         },
         "CT": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 55
+            "electoralVotes": 55,
+            "primaryDate": "April 26"
         },
         "AK": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 16
+            "electoralVotes": 16,
+            "primaryDate": "March 26"
         },
         "AR": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 32
+            "electoralVotes": 32,
+            "primaryDate": "March 1"
         },
         "AL": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 53
+            "electoralVotes": 53,
+            "primaryDate": "March 1"
         },
         "DC": {
             "fillKey": "UNDECIDED",
-            "electoralVotes": 53
+            "electoralVotes": 53,
+            "primaryDate": "June 14"
         }
     }
 });
